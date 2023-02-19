2d1e71d6b489ea264b458498ee58ea7ddec781fe
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-02-19 15:31:40 -0400

    feat: add import styles scss

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import '@/assets/styles.scss';
+
+import PrimeVue from 'primevue/config';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, { ripple: true });
+
 
 app.mount('#app')
