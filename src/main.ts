8bc3ed7e862735ba8c4ea8d75ad11751cec23a7a
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-02-20 08:22:37 -0400

    feat: import component DataTalbe and Column

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,16 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
 import '@/assets/styles.scss';
 
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+
 import PrimeVue from 'primevue/config';
+
+
+
+
 
 const app = createApp(App)
 
@@ -14,5 +20,7 @@ app.use(createPinia())
 app.use(router)
 app.use(PrimeVue, { ripple: true });
 
+app.component("DataTable", DataTable);
+app.component("Column", Column);
 
 app.mount('#app')
