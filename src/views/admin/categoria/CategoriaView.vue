d0829b0410909d338e21204d46c1f6e7c41836cc
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-02-20 09:13:20 -0400

    feat: button add categoria

--- src/views/admin/categoria/CategoriaView.vue
+++ src/views/admin/categoria/CategoriaView.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="card">
+    <Toolbar class="mb-4">
+      <template #start>
+        <Button label="Nueva Categoria" icon="pi pi-plus" class="p-button-success mr-2" @click="openNew" />
+      </template>
+
+      <template #end>
+        <FileUpload mode="basic" accept="image/*" :maxFileSize="1000000" label="Import" chooseLabel="Import"
+          class="mr-2 inline-block" />
+        <Button label="Export" icon="pi pi-upload" class="p-button-help" />
+      </template>
+    </Toolbar>
     <h5>Categoria</h5>
     <DataTable :value="lista_categorias" responsiveLayout="scroll" :loading="loading">
       <Column field="nombre" header="NOMBRE" :sortable="true"></Column>
@@ -15,6 +26,8 @@ import { onMounted, ref } from 'vue';
 
 const lista_categorias = ref([])
 const loading = ref(true)
+const submitted = ref(false)
+const productDialog = ref(false)
 
 onMounted(() => {
   ObtenerCategorias()
@@ -24,6 +37,13 @@ async function ObtenerCategorias() {
   const { data } = await apiCategoria.getCategoria();
   lista_categorias.value = data
   loading.value = false
+}
+
+function openNew() {
+  // lista_categorias.value = [];
+  // submitted.value = false;
+  // productDialog.value = true;
+  alert('Hola')
 }
 
 </script>
