0bcd3c68310620c5685b0c4166ef8096d63c4ee8
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-02-20 09:43:33 -0400

    feat: add modal for register new categoria

--- src/views/admin/categoria/CategoriaView.vue
+++ src/views/admin/categoria/CategoriaView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="card">
+    <!-- opciones de Categoria -->
     <Toolbar class="mb-4">
       <template #start>
         <Button label="Nueva Categoria" icon="pi pi-plus" class="p-button-success mr-2" @click="openNew" />
@@ -11,12 +12,34 @@
         <Button label="Export" icon="pi pi-upload" class="p-button-help" />
       </template>
     </Toolbar>
+    <!-- Tabla de categorias -->
     <h5>Categoria</h5>
     <DataTable :value="lista_categorias" responsiveLayout="scroll" :loading="loading">
       <Column field="nombre" header="NOMBRE" :sortable="true"></Column>
       <Column field="detalle" header="DETALLE" :sortable="true"></Column>
       <Column field="created_at" header="FECHA" :sortable="true"></Column>
     </DataTable>
+
+    <!-- Agregar nueva categoria -->
+    <Dialog v-model:visible="productDialog" :style="{ width: '450px' }" header="Registrar nueva categoria" :modal="true"
+      class="p-fluid">
+      <div class="field">
+        <label for="name">Nombre</label>
+        <InputText id="name" v-model.trim="categoria.nombre" required="true" autofocus
+          :class="{ 'p-invalid': submitted && !categoria.nombre }" />
+        <small class="p-error" v-if="submitted && !categoria.nombre">Name is required.</small>
+      </div>
+      <div class="field">
+        <label for="description">Detalle</label>
+        <Textarea id="description" v-model="categoria.detalle" required="true" rows="3" cols="20" />
+      </div>
+      <template #footer>
+        <Button label="Cancel" icon="pi pi-times" class="p-button-danger" @click="hideDialog" />
+        <Button label="Save" icon="pi pi-check" @click="saveCategory" />
+      </template>
+    </Dialog>
+
+
   </div>
 </template>
 
@@ -28,6 +51,10 @@ const lista_categorias = ref([])
 const loading = ref(true)
 const submitted = ref(false)
 const productDialog = ref(false)
+const categoria = ref({
+  nombre: '',
+  detalle: ''
+})
 
 onMounted(() => {
   ObtenerCategorias()
@@ -40,12 +67,22 @@ async function ObtenerCategorias() {
 }
 
 function openNew() {
-  // lista_categorias.value = [];
-  // submitted.value = false;
-  // productDialog.value = true;
-  alert('Hola')
+  categoria.value = {
+    nombre: '',
+    detalle: ''
+  };
+  submitted.value = false;
+  productDialog.value = true;
 }
 
+function hideDialog() {
+  productDialog.value = false;
+  submitted.value = false;
+}
+
+async function saveCategory() {
+
+}
 </script>
 
 <style></style>
