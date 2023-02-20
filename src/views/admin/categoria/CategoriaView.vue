d3ca2d140f28f6312356c96e9e454c5a8645bd7f
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-02-20 07:07:10 -0400

    feat: add Table Categoria

--- src/views/admin/categoria/CategoriaView.vue
+++ src/views/admin/categoria/CategoriaView.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>
-    <h3>Categoria</h3>
+  <div class="card">
+    <h5>Single Column</h5>
+    <DataTable :value="lista_categorias" responsiveLayout="scroll">
+      <Column field="nombre" header="Nombre" :sortable="true"></Column>
+      <Column field="detalle" header="Detalle" :sortable="true"></Column>
+      <Column field="created_at" header="Fecha de Creacion" :sortable="true"></Column>
+    </DataTable>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import * as apiCategoria from '@/services/categoria.service'
+import { onMounted, ref } from 'vue';
+
+const lista_categorias = ref([])
+
+
 
 </script>
 
