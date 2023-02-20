97847fd7bd37760149f68ccb57f53c711bc0bb8e
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-02-20 08:59:07 -0400

    feat: add getCategorias

--- src/views/admin/categoria/CategoriaView.vue
+++ src/views/admin/categoria/CategoriaView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card">
     <h5>Single Column</h5>
-    <DataTable :value="lista_categorias" responsiveLayout="scroll">
+    <DataTable :value="lista_categorias" responsiveLayout="scroll" :lazy="true">
       <Column field="nombre" header="Nombre" :sortable="true"></Column>
       <Column field="detalle" header="Detalle" :sortable="true"></Column>
       <Column field="created_at" header="Fecha de Creacion" :sortable="true"></Column>
@@ -14,8 +14,17 @@ import * as apiCategoria from '@/services/categoria.service'
 import { onMounted, ref } from 'vue';
 
 const lista_categorias = ref([])
+const loading = ref(false)
+const lazyParams = ref({})
 
+onMounted(() => {
+  ObtenerCategorias()
+})
 
+async function ObtenerCategorias() {
+  const { data } = await apiCategoria.getCategoria();
+  lista_categorias.value = data
+}
 
 </script>
 
