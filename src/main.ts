d542b1fdf4d5efd6a0694caf8e895e30704a9a85
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-02-20 22:00:15 -0400

    feat: import components vue

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,16 @@ import App from './App.vue'
 import router from './router'
 import '@/assets/styles.scss';
 
+import ToastService from "primevue/toastservice";
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
+import Toolbar from "primevue/toolbar";
+import Button from "primevue/button";
+import InputText from "primevue/inputtext";
+import Textarea from "primevue/textarea";
+import Dialog from "primevue/dialog";
+import FileUpload from "primevue/fileupload";
+import Toast from "primevue/toast";
 
 import PrimeVue from 'primevue/config';
 
@@ -19,8 +27,17 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(PrimeVue, { ripple: true });
+app.use(ToastService);
+
 
 app.component("DataTable", DataTable);
 app.component("Column", Column);
+app.component("Toolbar", Toolbar);
+app.component("Button", Button);
+app.component("InputText", InputText);
+app.component("Textarea", Textarea);
+app.component("Dialog", Dialog);
+app.component("FileUpload", FileUpload);
+app.component("Toast", Toast);
 
 app.mount('#app')
