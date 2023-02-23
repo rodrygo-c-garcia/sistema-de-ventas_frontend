71bd6e5a8a4c370a68ff61a3bb69fdfba24ef1c2
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-02-23 08:49:31 -0400

    feat: delete Proveedor of the proyect

--- src/layout/AppMenu.vue
+++ src/layout/AppMenu.vue
@@ -26,7 +26,6 @@ const model = ref([
     {
         label: 'Gestion de Usuario',
         items: [
-            { label: 'Proveedor', icon: 'pi pi-fw pi-prime', to: '/proveedor' },
             { label: 'Roles', icon: 'pi pi-fw pi-prime', to: '#' },
             { label: 'Usuarios', icon: 'pi pi-fw pi-desktop', to: '/usuario' }
         ]
