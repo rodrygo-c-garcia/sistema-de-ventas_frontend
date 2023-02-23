90cbcfa1aa48fa93d8f2a068ac3c909619a7fb3c
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-02-23 09:33:13 -0400

    feat: add table of show productos

--- src/views/admin/producto/ProductoView.vue
+++ src/views/admin/producto/ProductoView.vue
@@ -1,11 +1,63 @@
 <template>
   <div>
-    <h3>Producto</h3>
+    <div class="card">
+      <DataTable ref="dt" :value="productos" v-model:selection="selectedProducts" dataKey="id" :paginator="true"
+        :rows="10" :filters="filters"
+        paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+        :rowsPerPageOptions="[5, 10, 25]" currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
+        responsiveLayout="scroll">
+
+        <template #header>
+          <div class="table-header flex flex-column md:flex-row md:justiify-content-between">
+            <h5 class="mb-2 md:m-0 p-as-md-center">Administracion de Productos</h5>
+          </div>
+        </template>
+
+        <Column selectionMode="multiple" style="width: 3rem" :exportable="false"></Column>
+        <Column field="cod_barras" header="Codigo de barra" :sortable="true" style="min-width:10rem"></Column>
+        <Column field="nombre" header="Nombre" :sortable="true" style="min-width:12rem"></Column>
+        <!-- <Column header="Image">
+          <template #body="slotProps">
+            <img src="https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png" :alt="slotProps.data.image"
+              class="product-image" />
+          </template>
+        </Column> -->
+        <Column field="precio_compra" header="Precio Compra" :sortable="true" style="min-width:8rem">
+          <template #body="slotProps">
+            {{ formatCurrency(slotProps.data.price) }}
+          </template>
+        </Column>
+        <Column field="precio_venta" header="Precio Venta" :sortable="true" style="min-width:8rem">
+          <template #body="slotProps">
+            {{ formatCurrency(slotProps.data.price) }}
+          </template>
+        </Column>
+        <Column field="utilidad" header="Utilidad" :sortable="true" style="min-width:8rem">
+          <template #body="slotProps">
+            {{ formatCurrency(slotProps.data.price) }}
+          </template>
+        </Column>
+        <Column field="stock" header="Stock" :sortable="true" style="min-width:8rem"></Column>
+        <Column :exportable="false" style="min-width:8rem">
+          <template #body="slotProps">
+            <Button icon="pi pi-pencil" class="p-button-rounded p-button-success mr-2"
+              @click="editProduct(slotProps.data)" />
+            <Button icon="pi pi-trash" class="p-button-rounded p-button-warning"
+              @click="confirmDeleteProduct(slotProps.data)" />
+          </template>
+        </Column>
+      </DataTable>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
+import { FilterMatchMode } from 'primevue/api';
+import { useToast } from 'primevue/usetoast';
 
+const toast = useToast();
+const productos = ref([]);
 </script>
 
 <style></style>
