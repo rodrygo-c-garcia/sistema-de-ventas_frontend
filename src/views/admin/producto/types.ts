f4fb152bd806028131c45edf6352e72bb9e52fe5
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-02-26 11:03:08 -0400

    add: interface producto

---
+++ src/views/admin/producto/types.ts
@@ -0,0 +1,10 @@
+
+export interface Producto {
+    nombre: string;
+    cod_barras: string;
+    precio_compra: number;
+    precio_venta: number;
+    stock: number;
+    categoria_id: number;
+}
+
