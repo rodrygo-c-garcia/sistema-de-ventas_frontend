811cbb70b8a2d21d3a9824489be5e85da20a193c
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-03-01 19:21:10 -0400

    feat: add Toolbar Categoria

---
+++ src/views/admin/categoria/ToolbarCategoria.vue
@@ -0,0 +1,22 @@
+<template>
+  <Toolbar class="mb-4">
+    <template #start>
+      <Button label="Nueva Categoria" icon="pi pi-plus" class="p-button-success mr-2" @click="openNew" />
+    </template>
+    <template #end>
+      <h5>Categoria</h5>
+    </template>
+  </Toolbar>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<script lang="ts">
+export default {
+  name: 'ToolbarCategoria'
+}
+</script>
+
+<style></style>
