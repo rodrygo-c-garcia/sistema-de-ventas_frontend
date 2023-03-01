af2bb6518effc84eeaed8fee0066f258dce89266
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-03-01 19:27:09 -0400

    feat: add variables and functions

--- src/views/admin/categoria/ToolbarCategoria.vue
+++ src/views/admin/categoria/ToolbarCategoria.vue
@@ -10,7 +10,24 @@
 </template>
 
 <script setup lang="ts">
+import { ref, provide } from 'vue'
+import type { Categoria } from '../types';
 
+// Varibles
+const display = ref(false)
+provide('display', display)
+const categoria = ref(<Categoria>({}))
+
+// funciones
+function openNew(): void {
+  categoria.value = {
+    id: 0,
+    nombre: '',
+    detalle: ''
+  };
+  display.value = true
+  alert(display.value)
+}
 </script>
 
 <script lang="ts">
