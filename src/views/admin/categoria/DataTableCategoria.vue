38493899412758b6c68cc7994c6ddaf10c255def
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-03-01 19:16:45 -0400

    feat: getCategoy and integrate in Datatable

--- src/views/admin/categoria/DataTableCategoria.vue
+++ src/views/admin/categoria/DataTableCategoria.vue
@@ -1,7 +1,6 @@
 <template>
   <h5>Categoria</h5>
-  <DataTable ref="dt" :value="categorias" v-model:selection="selectedProducts" dataKey="id" :paginator="true" :rows="5"
-    :filters="filters" :loading="loading"
+  <DataTable ref="dt" :value="categorias" dataKey="id" :paginator="true" :rows="5" :filters="filters" :loading="loading"
     paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
     :rowsPerPageOptions="[5, 10, 25]" currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
     responsiveLayout="scroll">
@@ -21,15 +20,36 @@
 <script setup lang="ts">
 import type { Categoria } from '../types';
 import { ref, onMounted } from 'vue'
+import * as categoriaService from '@/services/categoria.service'
+import { FilterMatchMode } from 'primevue/api';
 
 // variables
-const categorias = ref(<Categoria>({}))
+const categorias = ref([])
+const categoria = ref(<Categoria>({}))
+const loading = ref(true)
+const filters = ref({
+  'global': { value: null, matchMode: FilterMatchMode.CONTAINS },
+});
+
+// Funciones
 
 onMounted(() => {
+  obtenerCategorias()
 })
 
+async function obtenerCategorias() {
+  const { data } = await categoriaService.getCategorias()
+  categorias.value = data
+  loading.value = false
+}
 
+function editCateory(cat: any): void {
+  alert('editCateory')
+}
 
+function confirmDeleteCategory(cat: any): void {
+  alert('deleteCateory')
+}
 </script>
 
 <script lang="ts">
