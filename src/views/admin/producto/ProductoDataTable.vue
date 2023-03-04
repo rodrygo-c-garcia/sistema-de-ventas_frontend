24366d1fdf3383c8f6498df08e712f99a92782c5
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-03-04 19:59:40 -0400

    feat: add column imagen

--- src/views/admin/producto/ProductoDataTable.vue
+++ src/views/admin/producto/ProductoDataTable.vue
@@ -14,26 +14,25 @@
     </template>
 
     <Column selectionMode="multiple" style="width: 3rem" :exportable="false"></Column>
-    <Column field="cod_barras" header="Codigo de barra" :sortable="true" style="min-width:10rem"></Column>
-    <Column field="nombre" header="Nombre" :sortable="true" style="min-width:12rem"></Column>
-    <Column field="categoria.nombre" header="Categoria" :sortable="true" style="min-width:12rem"></Column>
-    <!-- <Column header="Image">
-          <template #body="slotProps">
-            <img src="https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png" :alt="slotProps.data.image"
-              class="product-image" />
-          </template>
-        </Column> -->
-    <Column field="precio_compra" header="Precio Compra" :sortable="true" style="min-width:8rem">
+    <Column field="cod_barras" header="Codigo de barra" :sortable="true" style="min-width:8rem"></Column>
+    <Column field="nombre" header="Nombre" :sortable="true" style="min-width:8rem"></Column>
+    <Column field="categoria.nombre" header="Categoria" :sortable="true" style="min-width:8rem"></Column>
+    <Column header="Image">
+      <template #body="slotProps">
+        <img :src="`http://127.0.0.1:8000/${slotProps.data.imagen}`" :alt="slotProps.data.imagen" class="product-image" />
+      </template>
+    </Column>
+    <Column field="precio_compra" header="Precio Compra" :sortable="true" style="min-width:4rem">
       <template #body="slotProps">
         {{ formatCurrency(slotProps.data.precio_compra) }}
       </template>
     </Column>
-    <Column field="precio_venta" header="Precio Venta" :sortable="true" style="min-width:8rem">
+    <Column field="precio_venta" header="Precio Venta" :sortable="true" style="min-width:4rem">
       <template #body="slotProps">
         {{ formatCurrency(slotProps.data.precio_venta) }}
       </template>
     </Column>
-    <Column field="utilidad" header="Utilidad" :sortable="true" style="min-width:8rem">
+    <Column field="utilidad" header="Utilidad" :sortable="true" style="min-width:4rem">
       <template #body="slotProps">
         {{ formatCurrency(slotProps.data.utilidad) }}
       </template>
@@ -81,7 +80,6 @@ const filters = ref({
   'global': { value: null, matchMode: FilterMatchMode.CONTAINS },
 });
 
-
 // Funciones
 
 onMounted(() => {
@@ -96,6 +94,7 @@ async function obtenerProductos() {
     productos.value = prod
     loading.value = false
     actualizar_productos.value = false
+    console.log(productos.value)
   }
 }
 
@@ -122,4 +121,12 @@ export default {
   name: 'ProductoDataTable'
 }
 </script>
-<style></style>
+<style>
+.product-image {
+  height: 100px;
+  width: 100px;
+  object-fit: cover;
+  border-radius: 5px;
+  box-shadow: 0 2px 7px #000;
+}
+</style>
