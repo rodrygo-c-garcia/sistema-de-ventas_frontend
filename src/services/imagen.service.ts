108a9889fcdc6819a96e08f8677cb2bf5f59b9ae
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-03-05 17:58:24 -0400

    feat: add methods for conexion whit API ImgBB

---
+++ src/services/imagen.service.ts
@@ -0,0 +1,44 @@
+import { httpFile, http } from "./http.service";
+
+// API ImgBB
+export const getImageInfo = (id: any) => {
+  return httpFile().get(`image/${id}`)
+    .then(response => response.data)
+    .catch(error => {
+      console.error(`Error fetching image info: ${error}`);
+      throw error;
+    });
+};
+
+export const uploadIMG =  function(data: any ){
+   const formData = new FormData();
+  formData.append("image", data);
+
+  return httpFile().post(`upload`, formData)
+    .then(response => response.data)
+    .catch(error => {
+      console.error(`Error en carga de imagen: ${error}`);
+      throw error;
+    });
+}
+
+export const updateImage = (id: any, data: any) => {
+  return httpFile().put(`image/${id}`, data)
+    .then(response => response.data)
+    .catch(error => {
+      console.error(`Error updating image: ${error}`);
+      throw error;
+    });
+};
+
+export const deleteImage = (id: any) => {
+  return httpFile().delete(`image/${id}`)
+    .then(response => response.data)
+    .catch(error => {
+      console.error(`Error delete image: ${error}`);
+      throw error;
+    });
+};
+
+// Base de Datos
+
