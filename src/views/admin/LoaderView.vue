2d1109146aeb39fd3c0f55caadf80a1bfd7559f1
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-03-14 16:05:59 -0400

    feat: add component LoaderView

---
+++ src/views/admin/LoaderView.vue
@@ -0,0 +1,25 @@
+<template>
+  <Dialog v-model:visible="props.visible" :modal="true" class="p-fluid model-loading" :closable="false"
+    :style="{ width: '450px' }">
+    <p class="border">Cargando</p>
+    <p class="mave">Cargando</p>
+  </Dialog>
+  {{ props.msg }}
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps({
+  msg: { type: String },
+  visible: { type: Boolean }
+})
+
+</script>
+
+<script lang="ts">
+export default {
+  name: 'LoaderView'
+}
+</script>
+
+<style></style>
