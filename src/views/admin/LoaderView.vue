2b24f960da9ba3dc128aab692fc97941c7ca439e
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-03-16 09:49:21 -0400

    feat: add loading basic

--- src/views/admin/LoaderView.vue
+++ src/views/admin/LoaderView.vue
@@ -1,8 +1,10 @@
 <template>
   <Dialog v-model:visible="props.visible" :modal="true" class="p-fluid model-loading" :closable="false"
     :style="{ width: '450px' }">
-    <p class="border">Cargando</p>
-    <p class="mave">Cargando</p>
+    <div class="container-loading">
+      <div class="loader"></div>
+      <p>Cargando</p>
+    </div>
   </Dialog>
   {{ props.msg }}
 </template>
@@ -22,4 +24,25 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.container-loading {
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+}
+
+.loader {
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  border: 3px solid rgb(27, 20, 129);
+  border-top-color: transparent;
+  animation: spin 1.2s linear infinite;
+}
+
+@keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
