34edf2e4a7acdbcce34883603c240afa16cf5f2d
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-03-20 11:10:10 -0400

    feat: add get User of LocalStorage

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,16 @@
 <script setup lang="ts">
+import { usePinia } from './stores/store';
+const pinia = usePinia()
+
+const user = localStorage.getItem('user');
+if (user !== null) {
+  pinia.changeUser(JSON.parse(user));
+}
+
+// Borramos los items cada vez que se cierre la ventana (Caso de Cerrar la ventana)
+// localStorage.removeItem('token');
+// localStorage.removeItem('user');
+
 </script>
 
 <template>
