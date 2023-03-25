fc6c5fcdf55d5261cc9b231a181f52ba89b2aa77
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-03-24 21:49:22 -0400

    refactor: edit pinia

--- src/views/admin/pedido/NuevoPedidoView.vue
+++ src/views/admin/pedido/NuevoPedidoView.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
+  <div class="card">
     <h3>Nuevo Pedido</h3>
-    <h6>User: {{ pinia.user?.name }}</h6>
-    <h6>Email: {{ pinia.user?.email }}</h6>
-    {{ pinia.user }}
+
+  </div>
+  <div class="card">
+    carrito
   </div>
 </template>
 
 <script setup lang="ts">
-import { usePinia } from '@/stores/store'
-const pinia = usePinia();
+
 </script>
 
 <style></style>
