e614c561bc9557f195827dc670e4ac57a527db0e
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-04-07 15:36:39 -0400

    feat: define props

--- src/views/admin/pedido/DataTableCarrito.vue
+++ src/views/admin/pedido/DataTableCarrito.vue
@@ -1,8 +1,19 @@
 <template>
-  CARRITO
+  <h5>Carrito</h5>
+  {{ carrito }}
 </template>
 
 <script setup lang="ts">
+import { defineProps, ref, toRefs } from 'vue';
+
+const props = defineProps({
+  car: {
+    type: Object,
+    required: false
+  }
+})
+
+const { car: carrito } = toRefs(props)
 
 </script>
 
