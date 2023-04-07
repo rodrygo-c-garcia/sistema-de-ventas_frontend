8cfc6191dd6e60620178860cc0ae63fbb8bed2a5
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-04-07 15:35:32 -0400

    refactor: edit section cards

--- src/views/admin/pedido/NuevoPedidoView.vue
+++ src/views/admin/pedido/NuevoPedidoView.vue
@@ -1,11 +1,11 @@
 <template>
+  <!-- BUSCADOR -->
   <div class="card">
     <h3>Buscador</h3>
 
   </div>
-  <div class="card">
-    <DataTableProducto />
-  </div>
+  <!-- DATA TABLE -->
+  <DataTableProducto />
 </template>
 
 <script setup lang="ts">
