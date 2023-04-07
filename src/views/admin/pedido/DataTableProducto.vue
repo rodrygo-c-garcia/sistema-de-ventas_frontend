5307f53913bd41f0d0f036c783894f77650790c5
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-04-07 15:36:29 -0400

    feat: refactor DataTable producto and add funtion addStore

--- src/views/admin/pedido/DataTableProducto.vue
+++ src/views/admin/pedido/DataTableProducto.vue
@@ -1,46 +1,61 @@
 <template>
-  <DataTable ref="dt" :value="productos" dataKey="id" :paginator="true" :rows="5" :loading="loading"
-    paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
-    :rowsPerPageOptions="[5, 10, 25]" currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
-    responsiveLayout="scroll">
+  <div class="card">
+    <DataTable ref="dt" :value="productos" dataKey="id" :paginator="true" :rows="5" :loading="loading"
+      paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+      :rowsPerPageOptions="[5, 10, 25]" currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
+      responsiveLayout="scroll">
 
-    <template #header>
-      <div class="table-header flex flex-column md:flex-row md:justiify-content-between">
-        <h5 class="mb-2 md:m-0 p-as-md-center">Administracion de Productos</h5>
-      </div>
-    </template>
-
-    <Column field="cod_barras" header="Codigo de barra" :sortable="true" style="min-width:8rem"></Column>
-    <Column field="nombre" header="Nombre" :sortable="true" style="min-width:8rem"></Column>
-    <Column header="Imagen">
-      <template #body="slotProps">
-        <img :src="slotProps.data.imagen.url" class="product-image" alt="" />
+      <template #header>
+        <div class="table-header flex flex-column md:flex-row md:justiify-content-between">
+          <h5 class="mb-2 md:m-0 p-as-md-center">Administracion de Productos</h5>
+        </div>
       </template>
-    </Column>
-    <Column field="precio_venta" header="Precio Venta" :sortable="true" style="min-width:4rem">
-      <template #body="slotProps">
-        {{ formatCurrency(slotProps.data.precio_venta) }}
-      </template>
-    </Column>
-    <Column field="utilidad" header="Utilidad" :sortable="true" style="min-width:4rem">
-      <template #body="slotProps">
-        {{ formatCurrency(slotProps.data.utilidad) }}
-      </template>
-    </Column>
 
-    <Column field="stock" header="Stock" :sortable="true" style="min-width:5rem"></Column>
-    <Column :exportable="false" style="min-width:8rem">
+      <Column field="cod_barras" header="Codigo de barra" :sortable="true" style="min-width:8rem"></Column>
+      <Column field="nombre" header="Nombre" :sortable="true" style="min-width:8rem"></Column>
+      <Column header="Imagen">
+        <template #body="slotProps">
+          <img :src="slotProps.data.imagen.url" class="product-image" alt="" />
+        </template>
+      </Column>
+      <Column field="precio_venta" header="Precio Venta" :sortable="true" style="min-width:4rem">
+        <template #body="slotProps">
+          {{ formatCurrency(slotProps.data.precio_venta) }}
+        </template>
+      </Column>
+      <Column field="utilidad" header="Utilidad" :sortable="true" style="min-width:4rem">
+        <template #body="slotProps">
+          {{ formatCurrency(slotProps.data.utilidad) }}
+        </template>
+      </Column>
 
-    </Column>
-  </DataTable>
+      <Column field="stock" header="Stock" :sortable="true" style="min-width:5rem"></Column>
+      <Column :exportable="false" style="min-width:8rem">
+        <template #body="slotProps">
+          <Button icon="pi pi-cart-plus" class="p-button-rounded p-button-success mr-2"
+            @click="addStore(slotProps.data)" />
+        </template>
+      </Column>
+    </DataTable>
+  </div>
+  <!-- CARRITO -->
+  <div class="card">
+    <DataTableCarrito :car="carrito" />
+  </div>
 </template>
 
 <script setup lang="ts">
+import DataTableCarrito from './DataTableCarrito.vue';
 import * as serviceProducto from '@/services/producto.service'
 import { ref, onMounted } from 'vue';
 
-const productos = ref([])
+// loading
 const loading = ref(true)
+
+const productos = ref<Array<any>>([])
+
+const total_carrito = ref<number>(0.0)
+const carrito = ref<Array<any>>([])
 
 onMounted(() => {
   obtenerProductos()
@@ -58,6 +73,29 @@ const formatCurrency = (value: any) => {
     return value.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
   return null;
 };
+
+// Carrito 
+function addStore(producto: any) {
+  let prod: {
+    id: number;
+    nombre: string;
+    precio: number;
+    sub_total: number;
+    cantidad: number;
+  } = {
+    id: producto.id,
+    nombre: producto.nombre,
+    precio: producto.precio,
+    sub_total: 0.0,
+    cantidad: 1
+  };
+
+  // sacamos el subtotal de un producto
+  prod['sub_total'] = parseFloat((prod.precio * prod.cantidad).toFixed(2))
+
+  // Guardamos el producto en el arrglo
+  carrito.value.push(prod)
+}
 </script>
 
 <script lang="ts">
