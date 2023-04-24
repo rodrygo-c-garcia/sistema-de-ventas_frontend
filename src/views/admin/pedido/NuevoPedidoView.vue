59eeca46873e4070f90e07a4c69a33296703ed7a
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-04-23 21:12:55 -0400

    feat: add inputText for search product

--- src/views/admin/pedido/NuevoPedidoView.vue
+++ src/views/admin/pedido/NuevoPedidoView.vue
@@ -2,7 +2,11 @@
   <!-- BUSCADOR -->
   <div class="card">
     <h3>Buscador</h3>
-
+    <span class="p-input-icon-left">
+      <i class="pi pi-search" />
+      <!-- <InputText type="text" v-model="identificacion" placeholder="Buscar NIT del cliente" @keyup="getCliente" /> -->
+      <InputText style="width: 300px; height: 50px;" type="text" placeholder="Buscar NIT o nombre del Producto" />
+    </span>
   </div>
   <!-- DATA TABLE -->
   <DataTableProducto />
