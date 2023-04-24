d1d7490d8e6edf3ee3750f1b8953e66e5ffe5c61
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-04-23 21:29:10 -0400

    refactor: delete card search product

--- src/views/admin/pedido/NuevoPedidoView.vue
+++ src/views/admin/pedido/NuevoPedidoView.vue
@@ -1,19 +1,13 @@
 <template>
   <!-- BUSCADOR -->
-  <div class="card" style="display: flex; justify-content: center;">
-    <span class="p-input-icon-left">
-      <i class="pi pi-search" style="color: blue; font-size: 18px;" />
-      <!-- <InputText type="text" v-model="identificacion" placeholder="Buscar NIT del cliente" @keyup="getCliente" /> -->
-      <InputText style="width: 500px; height: 50px; box-shadow: 2px 1px 4px green;" type="text"
-        placeholder="Buscar NIT o nombre del Producto" />
-    </span>
-  </div>
+  <SearchProducto />
   <!-- DATA TABLE -->
   <DataTableProducto />
 </template>
 
 <script setup lang="ts">
 import DataTableProducto from './DataTableProducto.vue';
+import SearchProducto from './SearchProducto.vue';
 </script>
 
 <script lang="ts">
