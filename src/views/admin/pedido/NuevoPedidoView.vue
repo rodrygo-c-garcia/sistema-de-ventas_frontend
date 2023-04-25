a31ab3433629ea3afa27d153555e4a03a79601cb
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-04-24 20:57:07 -0400

    refactor: delete import componente SearchProducto

--- src/views/admin/pedido/NuevoPedidoView.vue
+++ src/views/admin/pedido/NuevoPedidoView.vue
@@ -1,13 +1,11 @@
 <template>
   <!-- BUSCADOR -->
-  <SearchProducto />
   <!-- DATA TABLE -->
   <DataTableProducto />
 </template>
 
 <script setup lang="ts">
 import DataTableProducto from './DataTableProducto.vue';
-import SearchProducto from './SearchProducto.vue';
 </script>
 
 <script lang="ts">
