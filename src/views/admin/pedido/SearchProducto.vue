c3a8fdf9dc7fa308078d4184811b6f2d4672c3de
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-04-27 08:09:35 -0400

    feat: add v-model in InputText

--- src/views/admin/pedido/SearchProducto.vue
+++ src/views/admin/pedido/SearchProducto.vue
@@ -3,7 +3,7 @@
     <span class="p-input-icon-left">
       <i class="pi pi-search" style="color: blue; font-size: 18px;" />
       <!-- <InputText type="text" v-model="identificacion" placeholder="Buscar NIT del cliente" @keyup="getCliente" /> -->
-      <InputText style="width: 500px; height: 50px; box-shadow: 2px 1px 4px green;" type="text"
+      <InputText style="width: 500px; height: 50px; box-shadow: 2px 1px 4px green;" type="text" v-model="search_term"
         placeholder="Buscar Codigo de Barra o nombre del Producto" @keyup="searchProduct()" />
     </span>
   </div>
