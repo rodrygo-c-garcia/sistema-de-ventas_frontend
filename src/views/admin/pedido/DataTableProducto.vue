d5ec65738ac736f9f024de21a37e250920fcafd8
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-04-28 20:36:45 -0400

    refactor: delete methods innesesary

--- src/views/admin/pedido/DataTableProducto.vue
+++ src/views/admin/pedido/DataTableProducto.vue
@@ -44,7 +44,7 @@
   </div>
   <!-- CARRITO -->
   <div class="card">
-    <DataTableCarrito :car="carrito" :prod="productos" :total_car="total_carrito" />
+    <DataTableCarrito :prod="producto" />
   </div>
 </template>
 
@@ -52,8 +52,7 @@
 import DataTableCarrito from './DataTableCarrito.vue';
 import SearchProducto from './SearchProducto.vue';
 
-import * as serviceProducto from '@/services/producto.service'
-import { ref, onMounted, provide } from 'vue';
+import { ref, provide } from 'vue';
 import { useToast } from 'primevue/usetoast';
 import type { CarritoItem, Producto } from '../types'
 
@@ -62,60 +61,21 @@ const loading = ref<boolean>(false);
 provide('loading', loading);
 
 const productos = ref<Array<Producto>>([]);
+const producto = ref({});
 
 const toast = useToast();
 
 
-const carrito = ref<Array<CarritoItem>>([])
-const total_carrito = ref<number>(0)
+
+// FUNCIONES
 
 // Carrito 
-function addStore(producto: Producto) {
-  let prod: CarritoItem = {
-    id: producto.id,
-    nombre: producto.nombre,
-    precio: producto.precio_venta,
-    sub_total: 0.0,
-    cantidad: 1
-  };
-
-  // sacamos el subtotal de un producto
-
-  prod['sub_total'] = parseFloat((prod.precio * prod.cantidad).toFixed(2))
-
-  // Buscamos el indice del elemento a reducir el stock
-  let index = productos.value.findIndex((prod: any) => prod.id === producto.id)
-
-
-  // Guardamos el producto en el arreglo de carrito, solo una vez por pruducto
-  if (!increaseProductQuantity(prod.id, index))
-    carrito.value.push(prod)
-
-  // preguntamos si existe el producto buscado y reducimos el stock que va a gregandoce
-  if (index !== -1) {
-    if (productos.value[index].stock === 0) toast.add({ severity: 'warn', summary: `Stock Vacio de ${productos.value[index].nombre}`, detail: 'Agregue mas productos de este tipo', life: 3000 });
-    else productos.value[index].stock--
-  }
-  increaseTotalCarrito()
+function addStore(prod: Producto) {
+  producto.value = prod;
+  console.log(producto.value)
 }
 
-function increaseProductQuantity(id: number, index: number) {
-  // retorna el indice del elemento a buscar
-  for (let prod = 0; prod < carrito.value.length; prod++) {
-    if (productos.value[index].stock === 0) return true
-    if (carrito.value[prod].id == id) {
-      carrito.value[prod].cantidad++
-      carrito.value[prod].sub_total = carrito.value[prod].cantidad * carrito.value[prod].precio
-      return true
-    }
-  }
-  return false
-}
 
-function increaseTotalCarrito() {
-  // sumamos todos los subtotales de los productos
-  total_carrito.value = carrito.value.reduce((total, producto) => total + producto.sub_total, 0)
-}
 
 const formatCurrency = (value: any) => {
   if (value)
