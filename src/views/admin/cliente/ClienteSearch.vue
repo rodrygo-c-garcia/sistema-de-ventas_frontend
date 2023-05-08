ac008fb1ea810f19b440ab26700d36fc696dd4c2
Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-05-07 21:40:13 -0400

    feat: add component ClienteSearch

---
+++ src/views/admin/cliente/ClienteSearch.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<script lang="ts">
+export default {
+  name: 'ClienteDialog',
+}
+</script>
+
+<style scoped></style>
