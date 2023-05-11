b10e8e4443621dd12d466e7c93e8a5be35f64f32
Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-05-11 12:46:41 -0400

    feat: add Dialog and the inject of the atribute visible

--- src/views/admin/pedido/RealizarPedido.vue
+++ src/views/admin/pedido/RealizarPedido.vue
@@ -1,11 +1,20 @@
 <template>
-  <div>
-
-  </div>
+  <Dialog v-model:visible="visible" modal header="Header" :style="{ width: '50vw' }">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+      aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+      sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+  </Dialog>
+  {{ visible }}
 </template>
 
 <script setup lang="ts">
+import { inject, ref } from 'vue';
 
+// ATRIBUTOS
+const visible = ref(inject<boolean>("visible"));
 </script>
 
 <script lang="ts">
