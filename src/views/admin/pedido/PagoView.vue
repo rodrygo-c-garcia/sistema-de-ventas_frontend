84b21a49d4bdf0ab92d31ff14788dd2b7dc3261f
Rodrigo <colqueariel023@gmail.com>
2023-06-12 08:07:55 -0400

    feat: add export component

--- src/views/admin/pedido/PagoView.vue
+++ src/views/admin/pedido/PagoView.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
-
+    <h2>Pago View</h2>
   </div>
 </template>
 
 <script setup lang="ts">
 
+</script>
+
+<script lang="ts">
+export default {
+  name: 'PagoView'
+}
 </script>
