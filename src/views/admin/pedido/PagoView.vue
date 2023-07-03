e2af9fdda5076f516f4d4b5fcef175b314b4d949
Rodrigo <colqueariel023@gmail.com>
2023-07-03 17:04:09 -0400

    feat: add form amount received

--- src/views/admin/pedido/PagoView.vue
+++ src/views/admin/pedido/PagoView.vue
@@ -1,10 +1,22 @@
 <template>
   <div>
-    <h2>Pago View</h2>
+    <div class="flex justify-content-left">
+      <div class="flex flex-column gap-2">
+        <label for="price">Cantidad recibida:</label>
+        <InputNumber inputId="horizontal" v-model="amount_received" showButtons buttonLayout="horizontal" :step="0.50"
+          decrementButtonClass="p-button-danger" incrementButtonClass="p-button-success" incrementButtonIcon="pi pi-plus"
+          decrementButtonIcon="pi pi-minus" mode="currency" currency="USD" locale="en-US" :min="0" :max="500" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+
+// Variables
+const amount_received = ref<number>(0);
+
 
 </script>
 
