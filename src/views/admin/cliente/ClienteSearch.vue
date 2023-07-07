c8eb5e794d4c64934e8267ad6ab9ee8b3c1a53ef
Rodrigo <colqueariel023@gmail.com>
2023-07-07 10:05:56 -0400

    feat: add Dialog register new Cliente

--- src/views/admin/cliente/ClienteSearch.vue
+++ src/views/admin/cliente/ClienteSearch.vue
@@ -1,10 +1,23 @@
 <template>
   <div>
-    <h2>Cliente Search</h2>
+    <h5>Cliente</h5>
+    <div class="card flex justify-content-center">
+      <Button label="Nuevo Cliente" icon="pi pi-external-link" @click="visible = true" />
+      <Dialog v-model:visible="visible" modal header="Header" :style="{ width: '50vw' }">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </Dialog>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+
 
 </script>
 
