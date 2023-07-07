33e982a0ae8dc633c4ac26e97d353bef79b3abbc
Rodrigo <colqueariel023@gmail.com>
2023-07-07 10:56:44 -0400

    feat: add inputText for search cliente

--- src/views/admin/cliente/SearchCliente.vue
+++ src/views/admin/cliente/SearchCliente.vue
@@ -1,4 +1,10 @@
-<template></template>
+<template>
+  <span class="p-input-icon-left">
+    <i class="pi pi-search" style="color: blue; font-size: 18px;" />
+    <InputText style="width: 300px; height: 50px; box-shadow: 2px 1px 4px green;" type="text"
+      placeholder="Buscar Cliente por NIT o Nombre" />
+  </span>
+</template>
 
 <script setup lang="ts">
 </script>
