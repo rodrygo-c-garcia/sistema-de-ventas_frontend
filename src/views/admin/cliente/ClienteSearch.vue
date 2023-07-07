dd45b60f6c8e1ce0100fd521525e763fb0a4dafd
Rodrigo <colqueariel023@gmail.com>
2023-07-07 10:29:17 -0400

    refactor: add delete Dialog and add structure

--- src/views/admin/cliente/ClienteSearch.vue
+++ src/views/admin/cliente/ClienteSearch.vue
@@ -2,27 +2,26 @@
   <div>
     <h5>Cliente</h5>
     <div class="card flex justify-content-center">
-      <Button label="Nuevo Cliente" icon="pi pi-external-link" @click="visible = true" />
-      <Dialog v-model:visible="visible" modal header="Header" :style="{ width: '50vw' }">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat.
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </Dialog>
+      <!-- Registrar nuevo cliente -->
+      <div>
+        <RegisterCliente />
+      </div>
+      <!-- Buscar Cliente -->
+      <div></div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+import RegisterCliente from './RegisterCliente.vue';
 
-
+// VARIABLES REACTIVAS
+const visible = ref<boolean>(false);
 </script>
 
 <script lang="ts">
 export default {
-  name: 'ClienteSearch',
+  name: "ClienteSearch"
 }
 </script>
