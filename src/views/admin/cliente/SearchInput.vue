14c254292161d026d39092913c87b0b1f55e732d
Rodrigo <colqueariel023@gmail.com>
2023-07-25 21:56:09 -0400

    feat: add componente SearchInput

---
+++ src/views/admin/cliente/SearchInput.vue
@@ -0,0 +1,13 @@
+<template>
+  <span class="p-input-icon-left">
+    <i class="pi pi-search" style="color: blue; font-size: 18px;" />
+    <InputText style="width: 300px; height: 50px; box-shadow: 2px 1px 4px green;" type="text"
+      placeholder="Buscar Cliente" />
+  </span>
+</template>
+
+<script lang="ts">
+export default {
+  name: "SearchInput"
+}
+</script>
