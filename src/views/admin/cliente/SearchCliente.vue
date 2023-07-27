cd73082244bd4685b29d3c86a626634fa103697b
Rodrigo <colqueariel023@gmail.com>
2023-07-27 18:20:21 -0400

    feat: add v-model and @keyup in InputText

--- src/views/admin/cliente/SearchCliente.vue
+++ src/views/admin/cliente/SearchCliente.vue
@@ -5,10 +5,11 @@
       <span class="p-input-icon-left">
         <i class="pi pi-search" style="color: blue; font-size: 18px;" />
         <InputText style="width: 300px; height: 50px; box-shadow: 2px 1px 4px green;" type="text"
-          placeholder="Buscar Cliente por NIT o Nombre" />
+          placeholder="Buscar Cliente por NIT o Nombre" v-model="searchTerm" @keyup="searchCustomer" />
       </span>
     </div>
     <div :style="{ margin: '10px' }">
+      {{ customers }}
       <CustomerWantedTable />
     </div>
   </Dialog>
