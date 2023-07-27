3f0f2153c28513963bdb740ad864f59932614f6c
Rodrigo <colqueariel023@gmail.com>
2023-07-27 15:41:32 -0400

    feat: add DataTable of the customer

--- src/views/admin/cliente/CustomerWantedTable.vue
+++ src/views/admin/cliente/CustomerWantedTable.vue
@@ -1,4 +1,10 @@
-<template></template>
+<template>
+  <DataTable tableStyle="min-width: 45vw">
+    <Column field="code" header="Code"></Column>
+    <Column field="name" header="Name"></Column>
+    <Column field="quantity" header="Quantity"></Column>
+  </DataTable>
+</template>
 
 <script lang="ts">
 export default {
