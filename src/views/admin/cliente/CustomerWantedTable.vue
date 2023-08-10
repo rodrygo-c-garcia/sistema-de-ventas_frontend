a179db0f204b29fbe03bc8703caf0e0f6c76a937
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-08-10 16:05:41 -0400

    feat: add inject and toast

--- src/views/admin/cliente/CustomerWantedTable.vue
+++ src/views/admin/cliente/CustomerWantedTable.vue
@@ -1,4 +1,5 @@
 <template>
+  <Toast />
   <DataTable :value="customers" tableStyle="min-width: 45vw" :loading="load">
     <Column field="nombre_completo" header="Nombre/Apellido"></Column>
     <Column field="nit" header="NIT"></Column>
@@ -8,6 +9,7 @@
           @click="addCustomer(slotProps.data)" />
       </template>
     </Column>
+    {{ visible }}
   </DataTable>
 </template>
 
@@ -15,6 +17,7 @@
 import { ref, toRefs, inject } from 'vue'
 import type { Cliente } from '../types'
 import { usePinia } from '@/stores/store';
+import { useToast } from 'primevue/usetoast';
 
 const props = defineProps({
   clientes: {
@@ -25,6 +28,7 @@ const props = defineProps({
 
 // VARIABLES
 const { clientes: customers } = toRefs(props);
+const toast = useToast();
 
 // VARIBLE DE PINIA
 const pinia = usePinia();
@@ -32,9 +36,15 @@ const pinia = usePinia();
 // injectamos el load para usarlo en el DataTable
 const load = ref(inject<boolean>('load'));
 
+// injectamos el visible para usarlo en el DataTable
+const visible = ref(inject<boolean>('visible'));
+
 // FUNCIONES
 function addCustomer(customer: Cliente): void {
   pinia.changeCustomer(customer);
+  visible.value = false;
+  toast.add({ severity: 'success', summary: "Cliente seleccionado", detail: '', life: 3000 });
+
 }
 </script>
 
